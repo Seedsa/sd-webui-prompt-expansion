8a91cd098bdf0cf6f2ad456d7dc749368546e30a
liji <896126514@qq.com>
2023-11-06 16:23:16 +0800

    first commit

--- README.md
+++ README.md
@@ -1,46 +1,7 @@
-# Prompt generator
-An extension for [webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui) that lets you generate prompts.
+# Prompt Expansion
 
-![](screenshot.png)
+download model from https://huggingface.co/lllyasviel/misc/resolve/main/fooocus_expansion.bin
 
-## Getting more models online
-You can add models from huggingface to the selection of models in setting. The setting field
-is `Hugginface model names for promptgen, separated by comma`, and its default value is
-just:
-```
-AUTOMATIC/promptgen-lexart, AUTOMATIC/promptgen-majinai-safe, AUTOMATIC/promptgen-majinai-unsafe
-```
-Those are GPT2 finetunes I did on various datasets:
+# API
 
-- [AUTOMATIC/promptgen-lexart](https://huggingface.co/AUTOMATIC/promptgen-lexart): Finetuned distilgpt2 for 100 epochs on prompts scraped from lexica.art
-- [AUTOMATIC/promptgen-majinai-safe](https://huggingface.co/AUTOMATIC/promptgen-majinai-safe): Finetuned distilgpt2 for 40 epochs on safe prompts scraped from majinai.art.
-- [AUTOMATIC/promptgen-majinai-unsafe](https://huggingface.co/AUTOMATIC/promptgen-majinai-unsafe): Finetuned distilgpt2 for 40 epochs on unsafe prompts scraped from majinai.art.
-
-To add more models, add more items to the setting, for example like this:
-
-```
-AUTOMATIC/promptgen-lexart, AUTOMATIC/promptgen-majinai-safe, AUTOMATIC/promptgen-majinai-unsafe, succinctly/text2image-prompt-generator, microsoft/Promptist
-```
-
-Reloading UI is required to apply this setting.
-
-## Getting more models offline
-Put your models into the `models` directort inside the extension. For example, my model can be
-made fully offline by placing fines from https://huggingface.co/AUTOMATIC/promptgen-lexart/tree/main into
-those directories:
-
-```
-
- 📁 webui root directory
- ┗━━ 📁 extensions
-     ┗━━ 📁 stable-diffusion-webui-promptgen
-         ┗━━ 📁 models                            
-             ┗━━ 📁 promptgen-lexart              <----- any name can be used
-                 ┣━━ 📄 config.json               <----- each model has its own set of required files;
-                 ┣━━ 📄 merges.txt                       mine requires all those
-                 ┣━━ 📄 pytorch_model.bin
-                 ┣━━ 📄 tokenizer_config.json
-                 ┗━━ 📄 vocab.json
-```
-
-Reloading UI is required to see new models.
+/prompt_expansion
--- scripts/promptgen.py
+++ scripts/promptgen.py
@@ -1,25 +1,27 @@
-import html
 import os
-import time
+from fastapi import FastAPI, Body
+import math
 
 import torch
 import transformers
 
-from modules import shared, generation_parameters_copypaste
-
-from modules import scripts, script_callbacks, devices, ui
+from modules import shared
+from transformers.generation.logits_process import LogitsProcessorList
+from transformers import  set_seed
+from modules import scripts, script_callbacks, devices
 import gradio as gr
 
-from modules.ui_components import FormRow
 
 
 class Model:
-    name = None
     model = None
     tokenizer = None
+    logits_bias = None
 
 
 available_models = []
+SEED_LIMIT_NUMPY = 2**32
+neg_inf = - 8192.0
 current = Model()
 
 base_dir = scripts.basedir()
@@ -30,214 +32,79 @@ def device():
     return devices.cpu if shared.opts.promptgen_device == 'cpu' else devices.device
 
 
-def list_available_models():
-    available_models.clear()
-
-    os.makedirs(models_dir, exist_ok=True)
-
-    for dirname in os.listdir(models_dir):
-        if os.path.isdir(os.path.join(models_dir, dirname)):
-            available_models.append(dirname)
-
-    for name in [x.strip() for x in shared.opts.promptgen_names.split(",")]:
-        if not name:
-            continue
-
-        available_models.append(name)
-
-
-def get_model_path(name):
-    dirname = os.path.join(models_dir, name)
-    if not os.path.isdir(dirname):
-        return name
-
+def get_model_path():
+    dirname = os.path.join(models_dir)
     return dirname
 
 
-def generate_batch(input_ids, min_length, max_length, num_beams, temperature, repetition_penalty, length_penalty, sampling_mode, top_k, top_p):
-    top_p = float(top_p) if sampling_mode == 'Top P' else None
-    top_k = int(top_k) if sampling_mode == 'Top K' else None
 
-    outputs = current.model.generate(
-        input_ids,
-        do_sample=True,
-        temperature=max(float(temperature), 1e-6),
-        repetition_penalty=repetition_penalty,
-        length_penalty=length_penalty,
-        top_p=top_p,
-        top_k=top_k,
-        num_beams=int(num_beams),
-        min_length=min_length,
-        max_length=max_length,
-        pad_token_id=current.tokenizer.pad_token_id or current.tokenizer.eos_token_id
-    )
-    texts = current.tokenizer.batch_decode(outputs, skip_special_tokens=True)
-    return texts
+def generate(text,seed, *args):
 
-
-def model_selection_changed(model_name):
-    if model_name == "None":
-        current.tokenizer = None
-        current.model = None
-        current.name = None
-
-        devices.torch_gc()
-
-
-def generate(id_task, model_name, batch_count, batch_size, text, *args):
-    shared.state.textinfo = "Loading model..."
-    shared.state.job_count = batch_count
-
-    if current.name != model_name:
-        current.tokenizer = None
-        current.model = None
-        current.name = None
-
-        if model_name != 'None':
-            path = get_model_path(model_name)
-            current.tokenizer = transformers.AutoTokenizer.from_pretrained(path)
-            current.model = transformers.AutoModelForCausalLM.from_pretrained(path)
-            current.name = model_name
+    path = get_model_path()
+    current.tokenizer = transformers.AutoTokenizer.from_pretrained(path)
+    current.model = transformers.AutoModelForCausalLM.from_pretrained(path)
 
     assert current.model, 'No model available'
     assert current.tokenizer, 'No tokenizer available'
 
     current.model.to(device())
+    seed = int(seed) % SEED_LIMIT_NUMPY
+    set_seed(seed)
+    positive_words = open(os.path.join(get_model_path(), 'positive.txt'),
+                              encoding='utf-8').read().splitlines()
+    positive_words = ['Ġ' + x.lower() for x in positive_words if x != '']
+    current.logits_bias = torch.zeros((1, len(current.tokenizer.vocab)), dtype=torch.float32) + neg_inf
 
-    shared.state.textinfo = ""
+    debug_list = []
+    for k, v in current.tokenizer.vocab.items():
+        if k in positive_words:
+            current.logits_bias[0, v] = 0
+            debug_list.append(k[1:])
+    print(f'Expansion: Vocab with {len(debug_list)} words.')
 
-    input_ids = current.tokenizer(text, return_tensors="pt").input_ids
-    if input_ids.shape[1] == 0:
-        input_ids = torch.asarray([[current.tokenizer.bos_token_id]], dtype=torch.long)
-    input_ids = input_ids.to(device())
-    input_ids = input_ids.repeat((batch_size, 1))
-
-    markup = '<table><tbody>'
-
-    index = 0
-    for i in range(batch_count):
-        texts = generate_batch(input_ids, *args)
-        shared.state.nextjob()
-        for generated_text in texts:
-            index += 1
-            markup += f"""
-<tr>
-<td>
-<div class="prompt gr-box gr-text-input">
-    <p id='promptgen_res_{index}'>{html.escape(generated_text)}</p>
-</div>
-</td>
-<td class="sendto">
-    <a class='gr-button gr-button-lg gr-button-secondary' onclick="promptgen_send_to_txt2img(gradioApp().getElementById('promptgen_res_{index}').textContent)">to txt2img</a>
-    <a class='gr-button gr-button-lg gr-button-secondary' onclick="promptgen_send_to_img2img(gradioApp().getElementById('promptgen_res_{index}').textContent)">to img2img</a>
-</td>
-</tr>
-"""
-
-    markup += '</tbody></table>'
-
-    return markup, ''
+    text = safe_str(text) + ','
+    tokenized_kwargs = current.tokenizer(text, return_tensors="pt")
+    tokenized_kwargs.data['input_ids'] = tokenized_kwargs.data['input_ids'].to(device())
+    tokenized_kwargs.data['attention_mask'] = tokenized_kwargs.data['attention_mask'].to(device())
+    current_token_length = int(tokenized_kwargs.data['input_ids'].shape[1])
+    max_token_length = 75 * int(math.ceil(float(current_token_length) / 75.0))
+    max_new_tokens = max_token_length - current_token_length
+    features = current.model.generate(**tokenized_kwargs,
+                                       top_k=100,
+                                       max_new_tokens=max_new_tokens,
+                                       do_sample=True,
+                                       logits_processor=LogitsProcessorList([logits_processor]))
+    response = current.tokenizer.batch_decode(features, skip_special_tokens=True)
+    result = safe_str(response[0])
+    return {'result':result}
 
 
-def find_prompts(fields):
-    field_prompt = [x for x in fields if x[1] == "Prompt"][0]
-    field_negative_prompt = [x for x in fields if x[1] == "Negative prompt"][0]
-    return [field_prompt[0], field_negative_prompt[0]]
+def logits_processor(input_ids, scores):
+    assert scores.ndim == 2 and scores.shape[0] == 1
+    current.logits_bias = current.logits_bias.to(scores)
 
+    bias = current.logits_bias.clone()
+    bias[0, input_ids[0].to(bias.device).long()] = neg_inf
+    bias[0, 11] = 0
+    return scores + bias
 
-def send_prompts(text):
-    params = generation_parameters_copypaste.parse_generation_parameters(text)
-    negative_prompt = params.get("Negative prompt", "")
-    return params.get("Prompt", ""), negative_prompt or gr.update()
+def safe_str(x):
+    x = str(x)
+    for _ in range(16):
+        x = x.replace('  ', ' ')
+    return x.strip(",. \r\n")
 
-
-def add_tab():
-    list_available_models()
-
-    with gr.Blocks(analytics_enabled=False) as tab:
-        with gr.Row():
-            with gr.Column(scale=80):
-                prompt = gr.Textbox(label="Prompt", elem_id="promptgen_prompt", show_label=False, lines=2, placeholder="Beginning of the prompt (press Ctrl+Enter or Alt+Enter to generate)").style(container=False)
-            with gr.Column(scale=10):
-                submit = gr.Button('Generate', elem_id="promptgen_generate", variant='primary')
-
-        with gr.Row(elem_id="promptgen_main"):
-            with gr.Column(variant="compact"):
-                selected_text = gr.TextArea(elem_id='promptgen_selected_text', visible=False)
-                send_to_txt2img = gr.Button(elem_id='promptgen_send_to_txt2img', visible=False)
-                send_to_img2img = gr.Button(elem_id='promptgen_send_to_img2img', visible=False)
-
-                with FormRow():
-                    model_selection = gr.Dropdown(label="Model", elem_id="promptgen_model", value=available_models[0], choices=["None"] + available_models)
-
-                with FormRow():
-                    sampling_mode = gr.Radio(label="Sampling mode", elem_id="promptgen_sampling_mode", value="Top K", choices=["Top K", "Top P"])
-                    top_k = gr.Slider(label="Top K", elem_id="promptgen_top_k", value=12, minimum=1, maximum=50, step=1)
-                    top_p = gr.Slider(label="Top P", elem_id="promptgen_top_p", value=0.15, minimum=0, maximum=1, step=0.001)
-
-                with gr.Row():
-                    num_beams = gr.Slider(label="Number of beams", elem_id="promptgen_num_beams", value=1, minimum=1, maximum=8, step=1)
-                    temperature = gr.Slider(label="Temperature", elem_id="promptgen_temperature", value=1, minimum=0, maximum=4, step=0.01)
-                    repetition_penalty = gr.Slider(label="Repetition penalty", elem_id="promptgen_repetition_penalty", value=1, minimum=1, maximum=4, step=0.01)
-
-                with FormRow():
-                    length_penalty = gr.Slider(label="Length preference", elem_id="promptgen_length_preference", value=1, minimum=-10, maximum=10, step=0.1)
-                    min_length = gr.Slider(label="Min length", elem_id="promptgen_min_length", value=20, minimum=1, maximum=400, step=1)
-                    max_length = gr.Slider(label="Max length", elem_id="promptgen_max_length", value=150, minimum=1, maximum=400, step=1)
-
-                with FormRow():
-                    batch_count = gr.Slider(label="Batch count", elem_id="promptgen_batch_count", value=1, minimum=1, maximum=100, step=1)
-                    batch_size = gr.Slider(label="Batch size", elem_id="promptgen_batch_size", value=10, minimum=1, maximum=100, step=1)
-
-                with open(os.path.join(base_dir, "explanation.html"), encoding="utf8") as file:
-                    footer = file.read()
-                    gr.HTML(footer)
-
-            with gr.Column():
-                with gr.Group(elem_id="promptgen_results_column"):
-                    res = gr.HTML()
-                    res_info = gr.HTML()
-
-        submit.click(
-            fn=ui.wrap_gradio_gpu_call(generate, extra_outputs=['']),
-            _js="submit_promptgen",
-            inputs=[model_selection, model_selection, batch_count, batch_size, prompt, min_length, max_length, num_beams, temperature, repetition_penalty, length_penalty, sampling_mode, top_k, top_p, ],
-            outputs=[res, res_info]
-        )
-
-        model_selection.change(
-            fn=model_selection_changed,
-            inputs=[model_selection],
-            outputs=[],
-        )
-
-        send_to_txt2img.click(
-            fn=send_prompts,
-            inputs=[selected_text],
-            outputs=find_prompts(ui.txt2img_paste_fields)
-        )
-
-        send_to_img2img.click(
-            fn=send_prompts,
-            inputs=[selected_text],
-            outputs=find_prompts(ui.img2img_paste_fields)
-        )
-
-    return [(tab, "Promptgen", "promptgen")]
-
-
-def on_ui_settings():
-    section = ("promptgen", "Promptgen")
-
-    shared.opts.add_option("promptgen_names", shared.OptionInfo("AUTOMATIC/promptgen-lexart, AUTOMATIC/promptgen-majinai-safe, AUTOMATIC/promptgen-majinai-unsafe", "Hugginface model names for promptgen, separated by comma", section=section))
-    shared.opts.add_option("promptgen_device", shared.OptionInfo("gpu", "Device to use for text generation", gr.Radio, {"choices": ["gpu", "cpu"]}, section=section))
 
 
 def on_unload():
     current.model = None
     current.tokenizer = None
 
+def prompt_expansion(_: gr.Blocks, app: FastAPI):
+    @app.post("/prompt_expansion")
+    async def expansion(prompt:str,seed:str):
+        result = generate(prompt,seed)
+        return  result
 
-script_callbacks.on_ui_tabs(add_tab)
-script_callbacks.on_ui_settings(on_ui_settings)
+script_callbacks.on_app_started(prompt_expansion)
 script_callbacks.on_script_unloaded(on_unload)
